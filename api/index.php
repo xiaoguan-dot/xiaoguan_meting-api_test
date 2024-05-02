d9d87a3ec3546fb53a039caacdb6c3ad98e5c579
Xiaoguan <100571246+xiaoguan0@users.noreply.github.com>
2024-05-02 08:30:13 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -43,8 +43,8 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET');
 
 include __DIR__ . '/vendor/autoload.php';
-// you can use 'Meting.php' instead of 'autoload.php'
-// include __DIR__ . '/Meting.php';
+you can use 'Meting.php' instead of 'autoload.php'
+include __DIR__ . '/Meting.php';
 
 use Metowolf\Meting;
 
